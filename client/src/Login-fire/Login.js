fd32ea59aa6669ac26ea45d847aea278ca9ca573
Yadhindra sri varshan <51809910+Yadhindrasrivarshan@users.noreply.github.com>
2021-06-14 17:23:02 +0530

    third commit

--- client/src/Login-fire/Login.js
+++ client/src/Login-fire/Login.js
@@ -1,50 +1,59 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Link } from 'react-router-dom'
-import  { useCallback, useContext } from 'react'; // add {useCallback, useContext}
-import {  Redirect } from "react-router";
-import {app} from '../mockfirebase.js'
-import { AuthContext } from "./Auth.js";
-
-
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Link } from "react-router-dom";
+import { useCallback, useContext } from "react"; // add {useCallback, useContext}
+import { app, provider } from "../mockfirebase.js";
+import { GoogleLogin } from "react-google-login";
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 }));
-export default function SignIn({history}) {
+
+export default function SignIn({ history }) {
   const classes = useStyles();
   const handleLogin = useCallback(
-    async event => {
+    async (event) => {
       event.preventDefault();
       const { email, password } = event.target.elements;
-  
       try {
         await app
           .auth()
           .signInWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
+  const signInWithGoogle = useCallback(
+    async (event) => {
+      event.preventDefault();
+      console.log("button clicked");
+      console.log(provider);
+      try {
+        await app.auth().signInWithPopup(provider);
         history.push("/");
       } catch (error) {
         alert(error);
@@ -57,9 +66,9 @@ export default function SignIn({history}) {
       <CssBaseline />
       <div className={classes.paper}>
         <Typography component="h1" variant="h5">
-         Employee Sign in
+          Employee Sign in
         </Typography>
-        <form onSubmit={handleLogin}  className={classes.form} noValidate>
+        <form onSubmit={handleLogin} className={classes.form} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
@@ -82,7 +91,7 @@ export default function SignIn({history}) {
             id="password"
             autoComplete="current-password"
           />
-         
+          ​
           <Button
             type="submit"
             fullWidth
@@ -100,10 +109,15 @@ export default function SignIn({history}) {
             </Grid>
           </Grid>
         </form>
+        ​<Typography>( OR )</Typography>
+        <Button
+          variant="contained"
+          onClick={signInWithGoogle}
+          color="secondary"
+        >
+          Sign In with google
+        </Button>
       </div>
-      
     </Container>
   );
 }
-
-
