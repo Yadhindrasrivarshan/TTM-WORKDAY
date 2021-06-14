1297b0640eba50ee3c556dcfd084743e14972a6e
Yadhindra sri varshan <51809910+Yadhindrasrivarshan@users.noreply.github.com>
2021-06-14 17:41:59 +0530

    Update mockfirebase.js

--- client/src/mockfirebase.js
+++ client/src/mockfirebase.js
@@ -1,12 +1,6 @@
 import firebase from "firebase";
 var firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyDsUXlick1r8r5g2OUczYLlZs1YrSjPJjo",
-  authDomain: "project-workday.firebaseapp.com",
-  projectId: "project-workday",
-  storageBucket: "project-workday.appspot.com",
-  messagingSenderId: "1020739224236",
-  appId: "1:1020739224236:web:c5418d436dcb4a2fc35509",
-  measurementId: "G-QGKJW87ZKB",
+  //add your own credentials
 });
 
 const provider = new firebase.auth.GoogleAuthProvider();
